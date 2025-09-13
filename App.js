633c07fea26a5561cbcdee8320bd82bfd30bc835
nikhil suthar <nikhilsuthar907@gmail.com>
2025-09-13 16:10:22 +0530

    first commit for vtrp-2 with all packages installed

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Inter_700Bold,
+  Inter_300Light,
+  Inter_500Medium,
+  Inter_400Regular,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
+import { IBMPlexSans_700Bold } from "@expo-google-fonts/ibm-plex-sans/700Bold";
+
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    Inter_700Bold,
+    Inter_300Light,
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_600SemiBold,
+    IBMPlexSans_700Bold,
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:"Inter_700Bold", color:"#6750A4"}}>My app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
